fdac9872d6d459a9304ae8261b6c97b624c37486
Vincent Rémond <vincent.remond@gmail.com>
2024-10-15 11:48:21 +0200

    Petit refactor

---
+++ ProcessGroup/Tools.fs
@@ -0,0 +1,25 @@
+﻿module ProcessGroup.Tools
+
+[<RequireQualifiedAccess>]
+module List =
+    let split predicate list =
+
+        List.foldBack
+            (fun x (matching, notMatching) ->
+                if predicate x then
+                    (x :: matching, notMatching)
+                else
+                    (matching, x :: notMatching)
+            )
+            list
+            ([], [])
+
+let rec (|NestedException|_|) (e: exn) =
+    match e with
+    | null -> None
+    | :? 'a as e -> Some e
+    | e -> (|NestedException|_|) e.InnerException
+
+let expect expected actual =
+    if expected <> actual then
+        failwithf $"Expected %A{expected} but got %A{actual}"
